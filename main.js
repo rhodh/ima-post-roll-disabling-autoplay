75b349947d9003cc12d962627a6c2232c75f3f64
rhodri.hewitson <rhodri.hewitson@nagra.com>
2019-01-16 18:04:47 +0000

    Swap stream to hls so not to confuse the dash autoplay issues on zap on videojs with post-roll defect.

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 const adTag = "https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&impl=s&gdfp_req=1&env=vp&output=vmap&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ar%3Dpostonly&cmsid=496&vid=short_onecue&correlator="
 
-const stream = "http://rdmedia.bbc.co.uk/dash/ondemand/bbb/2/client_manifest-common_init.mpd"
+const stream = "http://www.streambox.fr/playlists/test_001/stream.m3u8"
 
 let player = null;
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,13 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
 function playStream(){
 	const source = {
 		src: stream,
-		type: "application/dash+xml"
+		type: "application/x-mpegURL"
 	};
 
 	player.ima.changeAdTag(adTag);
     player.ima.requestAds();
 	player.src(source);
-    player.currentTime(570);
+    player.currentTime(895);
 }
-
 
